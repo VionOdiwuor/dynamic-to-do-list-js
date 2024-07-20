d85e1bd6875c55376444122d4f3c5345ee04b338
Rackel Odiwuor <odiwuorrackel@gmail.com>
2024-07-20 15:08:31 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded", (event) => {
+  const addButton = document.getElementById("add-task-btn");
+  const taskInput = document.getElementById("task-input");
+  const taskList = document.getElementById("task-list");
+  function addTask() {
+    const taskText = taskInput.value.trim();
+    if (taskText === "") {
+      alert("Please enter a Task");
+    } else {
+      const taskLi = document.createElement("li");
+      taskLi.textContent = taskText;
+      const remove = document.createElement("button");
+      remove.textContent = "Remove";
+      remove.className = "remove-btn";
+      remove.onclick = function () {
+        taskLi.remove();
+      };
+      taskLi.appendChild(remove);
+      taskList.appendChild(taskLi);
+    }
+  }
+  addButton.addEventListener("click", addTask);
+  taskInput.addEventListener("keypress", function (event) {
+    if (event.key === "Enter") {
+      addTask();
+    }
+  });
+  document.addEventListener("DOMContentLoaded", addTask);
+});
